1ca7266ba3bc98389a23f7f7e82bfd1bfa4058a0
Julio-cod33z <jcjrvn14@gmail.com>
2024-04-04 01:39:03 -0500

    objeto retrofit universal

--- app/src/main/java/com/example/apppatitasidatsjm/retrofit/PatitasCliente.kt
+++ app/src/main/java/com/example/apppatitasidatsjm/retrofit/PatitasCliente.kt
@@ -1,4 +1,26 @@
 package com.example.apppatitasidatsjm.retrofit
 
-class PatitasCliente {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object PatitasCliente {
+
+    private var retrofitCliente = OkHttpClient.Builder()
+        .connectTimeout(2, TimeUnit.MINUTES)
+        .readTimeout(30, TimeUnit.MINUTES)
+        .writeTimeout(15, TimeUnit.MINUTES)
+        //.addInterceptor()
+        .build()
+
+    private fun buildRetrofit() = Retrofit.Builder()
+        .baseUrl("http://www.kreapps.biz/patitas/")
+        .client(retrofitCliente)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val retrofitService: PatitasServicio by lazy {
+        buildRetrofit().create(PatitasServicio::class.java)
+    }
 }
--- app/src/main/java/com/example/apppatitasidatsjm/retrofit/PatitasServicio.kt
+++ app/src/main/java/com/example/apppatitasidatsjm/retrofit/PatitasServicio.kt
@@ -1,4 +1,28 @@
 package com.example.apppatitasidatsjm.retrofit
 
+import com.example.apppatitasidatsjm.retrofit.request.LoginRequest
+import com.example.apppatitasidatsjm.retrofit.request.RegistroRequest
+import com.example.apppatitasidatsjm.retrofit.request.VoluntarioRequest
+import com.example.apppatitasidatsjm.retrofit.response.LoginResponse
+import com.example.apppatitasidatsjm.retrofit.response.MascotaResponse
+import com.example.apppatitasidatsjm.retrofit.response.RegistroResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+
 interface PatitasServicio {
+
+    @POST("login.php")
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+
+    @PUT("persona.php")
+    fun registro(@Body registroRequest: RegistroRequest): Call<RegistroResponse>
+
+    @POST("personavoluntaria.php")
+    fun voluntario(@Body voluntarioRequest: VoluntarioRequest): Call<RegistroResponse>
+
+    @GET("mascotaperdida.php")
+    fun mascotas(): Call<List<MascotaResponse>>
 }
