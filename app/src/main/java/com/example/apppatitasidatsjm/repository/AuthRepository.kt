7b78df925463e9616158cd9d68f19624c4b0a836
Julio-cod33z <jcjrvn14@gmail.com>
2024-04-04 01:57:58 -0500

    AuthRepository terminado

--- app/src/main/java/com/example/apppatitasidatsjm/repository/AuthRepository.kt
+++ app/src/main/java/com/example/apppatitasidatsjm/repository/AuthRepository.kt
@@ -1,4 +1,48 @@
 package com.example.apppatitasidatsjm.repository
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.apppatitasidatsjm.retrofit.PatitasCliente
+import com.example.apppatitasidatsjm.retrofit.request.LoginRequest
+import com.example.apppatitasidatsjm.retrofit.request.RegistroRequest
+import com.example.apppatitasidatsjm.retrofit.response.LoginResponse
+import com.example.apppatitasidatsjm.retrofit.response.RegistroResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 class AuthRepository {
+
+    var loginResponse = MutableLiveData<LoginResponse>()
+    var registroResponse = MutableLiveData<RegistroResponse>()
+
+    fun autenticarUsuario(loginRequest: LoginRequest): MutableLiveData<LoginResponse>
+    {
+        val call: Call<LoginResponse> = PatitasCliente.retrofitService.login(loginRequest)
+        call.enqueue(object : Callback<LoginResponse> {
+            override fun onResponse(p0: Call<LoginResponse>, p1: Response<LoginResponse>) {
+                loginResponse.value = p1.body()
+            }
+
+            override fun onFailure(p0: Call<LoginResponse>, p1: Throwable) {
+                Log.e( "ErrorLogin", p1.message.toString())
+            }
+        })
+        return loginResponse
+    }
+
+    fun registrarUsuario(registroRequest: RegistroRequest): MutableLiveData<RegistroResponse>
+    {
+        val call: Call<RegistroResponse> = PatitasCliente.retrofitService.registro(registroRequest)
+        call.enqueue(object : Callback<RegistroResponse> {
+            override fun onResponse(p0: Call<RegistroResponse>, p1: Response<RegistroResponse>) {
+                registroResponse.value = p1.body()
+            }
+
+            override fun onFailure(p0: Call<RegistroResponse>, p1: Throwable) {
+                Log.e( "ErrorRegistro", p1.message.toString())
+            }
+        })
+        return registroResponse
+    }
 }
