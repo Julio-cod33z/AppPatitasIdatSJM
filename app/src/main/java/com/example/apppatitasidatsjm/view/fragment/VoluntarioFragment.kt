21be8d78a1363ea2819e9a5de39bb8e190aca56c
Julio-cod33z <jcjrvn14@gmail.com>
2024-04-08 13:06:20 -0500

    avance de registro de voluntario

--- app/src/main/java/com/example/apppatitasidatsjm/view/fragment/VoluntarioFragment.kt
+++ app/src/main/java/com/example/apppatitasidatsjm/view/fragment/VoluntarioFragment.kt
@@ -5,17 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.apppatitasidatsjm.R
+import androidx.lifecycle.ViewModelProvider
+import com.example.apppatitasidatsjm.databinding.FragmentVoluntarioBinding
+import com.example.apppatitasidatsjm.util.AppMensaje
+import com.example.apppatitasidatsjm.util.TipoMensaje
+import com.example.apppatitasidatsjm.viewmodel.VoluntarioViewModel
 
-class VoluntarioFragment : Fragment() {
+class VoluntarioFragment : Fragment(), View.OnClickListener {
+
+    var _binding: FragmentVoluntarioBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var voluntarioViewModel: VoluntarioViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_voluntario, container, false)
+        _binding = FragmentVoluntarioBinding.inflate(inflater, container, false)
+        voluntarioViewModel = ViewModelProvider(requireActivity())
+            .get(VoluntarioViewModel::class.java)
+        binding.btnRegistrarVoluntario.setOnClickListener(this)
+        return binding.root
     }
 
-
+    override fun onClick(v: View?) {
+        if(binding.cbAceptarTerminos.isChecked) {
+            binding.btnRegistrarVoluntario.isEnabled = false
+            //voluntarioViewModel.registrarVoluntario()
+        } else {
+            AppMensaje.enviarMensaje(binding.root,
+                "Acepte los términos y condiciones para ser voluntario",
+                TipoMensaje.ERROR)
+        }
+    }
 }
